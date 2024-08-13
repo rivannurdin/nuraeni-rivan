37947e4d8c828cf4443404765f2f5f1f7f8638ff
Rivan Nurdin <rivancodes@gmail.com>
2024-08-13 10:27:50 +0700

    update intro

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -11,7 +11,7 @@ const count = ref(0)
 <template>
     <div class="intro text-light py-32 font-light text-zinc-200">
         <h1 class="text-4xl tracking-wide leading-relaxed my-20">{{ msg }}</h1>
-        <h2 class="text-4xl tracking-wide leading-relaxed my-20 max-w-3xl">I'm <span class="font-bold">Rivan Nurdin</span>, a back-end software engineer with a talent for developing efficient systems and robust infrastructures that enhance seamless user experiences 😎</h2>
+        <h2 class="text-4xl tracking-wide leading-relaxed my-20 max-w-3xl">I'm <span class="font-bold">Bambang</span>, a back-end software engineer with a talent for developing efficient systems and robust infrastructures that enhance seamless user experiences 😎</h2>
         <h3 class="text-xl my-20">Get in touch 👉 <a class="text" href="mailto:rivancodes@gmail.com">rivancodes@gmail.com</a></h3>
     </div>
 </template>
