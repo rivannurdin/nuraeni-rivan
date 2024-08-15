dfd742be37170b39fc93dd0f74d0186014b11153
Rivan Nurdin <rivancodes@gmail.com>
2024-08-15 21:40:38 +0700

    Create skills components

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -9,10 +9,10 @@ const count = ref(0)
 </script>
 
 <template>    
-    <div class="intro text-light py-32 font-light text-zinc-200">
+    <div class="intro py-20 md:py-32 lg:py-32 font-light text-zinc-200">
         <p class="text-2xl md:text-3xl lg:text-4xl tracking-wide md:tracking-wide lg:tracking-wide leading-relaxed md:leading-relaxed lg:leading-relaxed my-20">{{ msg }}</p> 
-        <p class="text-2xl md:text-3xl lg:text-4xl tracking-wide md:tracking-wide lg:tracking-wide leading-relaxed md:leading-relaxed lg:leading-relaxed my-20 max-w-3xl">I'm <span class="font-bold">Rivan Nurdin</span>, a back-end software engineer with a talent for developing efficient systems and robust infrastructures that enhance seamless user experiences 😎</p> 
-        <p class="text-lg md:text-lg lg:text-xl my-20">Get in touch 👉 <a class="text" href="mailto:rivancodes@gmail.com">rivancodes@gmail.com</a></p>
+        <p class="text-2xl md:text-3xl lg:text-4xl tracking-wide md:tracking-wide lg:tracking-wide leading-relaxed md:leading-relaxed lg:leading-relaxed my-20 max-w-3xl">I'm <span class="font-medium">Rivan Nurdin</span>, a back-end software engineer with a talent for developing efficient systems and robust infrastructures that enhance seamless user experiences 😎</p> 
+        <p class="text-lg md:text-lg lg:text-xl my-20">Get in touch 👉 <a href="mailto:rivancodes@gmail.com">rivancodes@gmail.com</a></p>
     </div>
 </template>
 
---
+++ src/components/Skills.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>    
+    <div class="skills py-20 md:py-32 lg:py-32">
+      <div class="grid grid-cols-3 lg:grid-cols-4">
+        <div class="text-teal-300 text-left lg:text-right font-medium mb-5 lg:mb-0 row-span-0 lg:row-span-2 col-span-3 lg:col-auto">SKILLS</div>
+        <div class="px-5 lg:px-10 mb-5 lg:mb-0 ">
+          <p class="mb-2 text-zinc-100 font-medium">BACK-END</p>
+          <p>PHP (Laravel, Lumen, Phalcon)</p>
+          <p>Python (Fastapi, Django)</p>
+          <p>NodeJS (Expressjs)</p>
+        </div>
+        <div class="px-5 md:px-5 lg:px-10">
+          <p class="mb-2 text-zinc-100 font-medium">FRONT-END</p>
+          <p>JQuery</p>
+          <p>Bootstrap/ Tailwind</p>
+          <p>React/ Vue</p>
+        </div>
+        <div class="px-5 lg:px-10">
+          <p class="mb-2 text-zinc-100 font-medium">DATABASE</p>
+          <p>MySQL</p>
+          <p>PostgreSQL</p>
+          <p>SQL Server</p>
+        </div>
+        <div class="px-5 lg:px-10 col-span-3 pt-5">
+          <p class="mb-2 text-zinc-100 font-medium">TOOLS & PLATFORM</p>
+          <p>Apache, Azure, Firebase, Figma, Flowchart, GIT, Github, Gitlab, Google Docs, Google Spreadsheets, Jira, pgAdmin4, Postman, Project/Product Management, REST APIs, S3 Storage, Scrum, Swagger, Trello, Wordpress</p>
+        </div>
+      </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
