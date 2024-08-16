2ba2d19ef52cbef6b8640068a4415c41a6b33702
Rivan Nurdin <rivancodes@gmail.com>
2024-08-16 19:46:37 +0700

    update

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,7 +4,7 @@
 <template>    
     <div class="about py-12 lg:py-30">
       <div class="grid grid-cols-4">
-        <div class="text-teal-300 text-left lg:text-right font-medium mb-5 lg:mb-0 col-span-4 lg:col-auto">BACKGOUND</div>
+        <div class="text-teal-300 text-left lg:text-right font-medium mb-5 lg:mb-0 col-span-4 lg:col-auto">BACKGROUND</div>
         <div class="col-span-4 lg:col-span-3 px-5 lg:px-10 max-w-3xl">
           <p class="mb-5">I'm currently an engineer at <a href="https://www.gravitasi.co.id" target="_blank">Gravitasi</a>, where I contribute to developing and maintenance the <a href="https://www.gravitasi.co.id/portfolio/kmob-pemda-depok/" target="_blank">KMOB Platform</a> for the Depok City Government and I recently graduated in 2024 with a Bachelor of Computer Science (B.C.Sc.) degree in Information Technology from <a href="https://ars.ac.id" target="_blank">ARS University</a>.</p>
           <p class="mb-5">I embarked on my journey as a Backend Developer in 2017, and over the past six years, I've honed my expertise in crafting robust management APIs. Currently, I'm expanding my skill set by diving into cutting-edge technologies like Python and Node.js.</p>
