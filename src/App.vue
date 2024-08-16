f5bad544932d2f28370052834e3915d8af45f72a
Rivan Nurdin <rivancodes@gmail.com>
2024-08-16 08:52:49 +0700

    add social media icon

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
 import Intro from './components/Intro.vue';
-import Header from './components/Header.vue';
 import About from './components/About.vue';
 import Skill from './components/Skills.vue'
 import Experience from './components/Experience.vue'
@@ -11,8 +9,6 @@ import Projects from './components/Projects.vue';
 </script>
 
 <template>
-  <!-- <div class="pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute" style="background: radial-gradient(600px at 73px 697px, rgba(29, 78, 216, 0.15), transparent 80%);"></div> -->
-  <Header msg="Hello! 👋" />
   <div class="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
     <Intro msg="Hello! 👋" />
     <About/>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,7 +9,8 @@ const count = ref(0)
 </script>
 
 <template>
-    <header class="intro">
+    <header class="mx-auto max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+      
     </header>
 </template>
 
