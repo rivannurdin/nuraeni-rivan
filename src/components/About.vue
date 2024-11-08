c5c43cae727571e0e0334c6c40676ca8591df890
Rivan N. <rivancodes@gmail.com>
2024-11-08 17:13:42 +0700

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -8,7 +8,7 @@
         <div class="col-span-4 lg:col-span-3 px-5 lg:px-10 max-w-3xl">
           <p class="mb-5">I'm currently an engineer at <a href="https://www.gravitasi.co.id" target="_blank">Gravitasi</a>, where I contribute to developing and maintenance the <a href="https://www.gravitasi.co.id/portfolio/kmob-pemda-depok/" target="_blank">KMOB Platform</a> for the Depok City Government and I recently graduated in 2024 with a Bachelor of Computer Science (B.C.Sc.) degree in Information Technology from <a href="https://ars.ac.id" target="_blank">ARS University</a>.</p>
           <p class="mb-5">I embarked on my journey as a Backend Developer in 2017, and over the past six years, I've honed my expertise in crafting robust management APIs. Currently, I'm expanding my skill set by diving into cutting-edge technologies like Python and Node.js.</p>
-          <p>I love to share my thoughts and creativity with the world, whether it's through thought-provoking articles on <a href="https://rivannurdin.medium.com/">Medium</a> or captivating stories and poetry on <a href="https://www.wattpad.com/user/rivannurdin">Wattpad</a>.</p>
+          <p>Crafting stories at <a href="https://www.instagram.com/mortalveil.art" target="_blank">Mortalveil</a>, a Punk Masterpiece, is where I channel creativity into rebellious narratives and innovative projects. It embodies a fusion of raw energy and thought-provoking content.</p>
         </div>
       </div>
     </div>
