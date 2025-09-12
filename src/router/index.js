5e50ba56926a59310da2f2872329e4e3502be518
Rivan Nurdin <rivancodes@gmail.com>
2025-09-12 10:04:31 +0700

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../App.vue'
-import RSVPList from '../pages/RsvpList.vue'
+import RsvpList from '../pages/RsvpList.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: Home },
-    { path: '/rsvp-list', component: RSVPList },
+    { path: '/rsvp-list', component: RsvpList },
   ],
 })
 
